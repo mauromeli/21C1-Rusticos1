a43f0112a735025ec39b4add0c614f131e5ca0c3
Mauro Di Pietro <mauro.pietro@mercadolibre.com>
2021-06-20 01:32:53 -0300

    Methods reorg

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use crate::config::server_config::Config;
 use crate::service::server::Server;
 
+mod config;
 mod entities;
 mod service;
 
 fn main() {
-    let server = Server::new();
+    let config = Config::new();
+    let server = Server::new(config);
     server.serve();
     println!("Hello, world!");
 }
--- src/service/server.rs
+++ src/service/server.rs
@@ -1,105 +1,100 @@
-use crate::service::redis::Redis;
-use std::net::{TcpListener, TcpStream};
-use std::io::{BufReader, BufRead, Write};
-use std::sync::mpsc;
-use std::thread;
-use crate::service::command_generator::generate;
-use std::sync::mpsc::{Sender, Receiver};
+use crate::config::server_config::Config;
 use crate::entities::command::Command;
+use crate::service::command_generator::generate;
+use crate::service::redis::Redis;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
 
 #[derive(Debug)]
 pub struct Server {
     redis: Redis,
+    config: Config,
 }
 
 impl Server {
     #[allow(dead_code)]
-    pub fn new() -> Self {
-        //TODO: Add config to constructor
+    pub fn new(config: Config) -> Self {
         let redis = Redis::new();
 
-        Self { redis }
+        Self { redis, config }
     }
 
-    pub fn serve(mut self) {
-        //TODO: leer de config
-        let address = "0.0.0.0:".to_owned() + "8080";
+    pub fn serve(self) {
+        let address = "0.0.0.0:".to_owned() + self.config.clone().get_port().as_str();
         self.server_run(&address).unwrap();
     }
 
-
-    //TODO: Hay forma de meterlo en el mismo struct?
-    fn server_run(mut self, address: &str) -> std::io::Result<()> {
+    fn server_run(self, address: &str) -> std::io::Result<()> {
         let listener = TcpListener::bind(address)?;
-
         let (db_sender, db_receiver) = mpsc::channel();
 
-        let db_thread = thread::spawn(move ||
-            {
-                while let msg = db_receiver.recv() {
-                    if msg.is_err() {
-                        panic!();
-                    }
-                    let (command, sender): (Command, Sender<String>) = msg.unwrap();
-                    //TODO: Ver que hacer en DB
-                    let redis_response = self.redis.execute(command);
-
-                    println!("{:?}", redis_response);
-                    sender.send(redis_response.unwrap().to_string());
-
-                    //TODO: Encode RedisResponse
-                }
-            }
-        );
+        self.db_thread(db_receiver);
 
         while let connection = listener.accept()? {
             let (client, _) = connection;
             let db_sender_clone = db_sender.clone();
-            let handler = thread::spawn(move || {
-                let client_input: TcpStream = client.try_clone().unwrap();
-                let client_output: TcpStream = client;
-
-                //handle_client(&mut client_stream);
+            let _ = thread::spawn(move || {
                 //TODO: Mover
-
-                let input = BufReader::new(client_input);
-                let mut output = client_output;
-                let mut lines = input.lines();
-
-                // iteramos las lineas que recibimos de nuestro cliente
-                while let Some(request) = lines.next() {
-
-                    let (client_sender, client_receiver): (Sender<String>, Receiver<String>) = mpsc::channel();
-
-                    //TODO: Agregar decode
-                    let mut vector: Vec<String> = vec![];
-                    for string in request.unwrap().split(" ") {
-                        vector.push(string.to_string())
-                    }
-                    println!("{:?}", vector);
-                    let command = generate(vector);
-
-                    println!("{:?}", command);
-
-                    match command {
-                        Ok(command) => {
-                            db_sender_clone.send((command, client_sender));
-                        }
-                        _ => {
-                            output.write("Error".to_string().as_ref());
-                        }
-                    };
-
-                    let response = client_receiver.recv();
-                    println!("{:?}",response);
-                    let nbytes = output.write(response.unwrap().as_bytes());
-                    output.write("\n".as_bytes());
-                    println!("nbytes: {:?}", nbytes);
-                }
-
+                Server::client_handler(client, db_sender_clone)
                 //TODO: Mover
             });
         }
         Ok(())
+    }
+
+    fn client_handler(client: TcpStream, db_sender_clone: Sender<(Command, Sender<String>)>) {
+        let client_input: TcpStream = client.try_clone().unwrap();
+        let client_output: TcpStream = client;
+        let input = BufReader::new(client_input);
+        let mut output = client_output;
+        let mut lines = input.lines();
+
+        // iteramos las lineas que recibimos de nuestro cliente
+        while let Some(request) = lines.next() {
+            let (client_sender, client_receiver): (Sender<String>, Receiver<String>) =
+                mpsc::channel();
+
+            //TODO: Agregar decode
+            let mut vector: Vec<String> = vec![];
+            for string in request.unwrap().split(" ") {
+                vector.push(string.to_string())
+            }
+            //TODO: FIN Agregar decode
+
+            let command = generate(vector);
+            let output_response;
+
+            // TODO: Agregar forma de escritura
+            match command {
+                Ok(command) => {
+                    let _ = db_sender_clone.send((command, client_sender));
+                    let response = client_receiver.recv();
+                    output_response = response.unwrap() + "\n";
+                }
+                _ => {
+                    output_response = command.err().unwrap() + "\n";
+                }
+            };
+
+            let _ = output.write(output_response.as_ref());
+        }
+    }
+
+    fn db_thread(mut self, db_receiver: Receiver<(Command, Sender<String>)>) {
+        let _ = thread::spawn(move || {
+            while let msg = db_receiver.recv() {
+                if msg.is_err() {
+                    panic!();
+                }
+
+                let (command, sender): (Command, Sender<String>) = msg.unwrap();
+                let redis_response = self.redis.execute(command);
+                //TODO: Encode RedisResponse
+                let _ = sender.send(redis_response.unwrap().to_string());
+            }
+        });
     }
 }
