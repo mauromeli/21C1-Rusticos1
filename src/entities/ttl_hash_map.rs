de98325ffd314bf6fca5bf3df006f2c940e0d659
tomasarrachea <tarrachea@fi.uba.ar>
2021-07-20 03:49:21 -0300

    TtlHashMap tests

--- src/entities/ttl_hash_map.rs
+++ src/entities/ttl_hash_map.rs
@@ -36,11 +36,7 @@ impl<K: Clone + Eq + Hash, V> TtlHashMap<K, V> {
 
         self.last_access
             .insert(key.clone(), SystemTime::now())
-            .map(|value| {
-                value
-                    .duration_since(SystemTime::now())
-                    .unwrap_or_else(|_| Duration::from_secs(0))
-            })
+            .map(|value| value.elapsed().unwrap_or_else(|_| Duration::from_secs(0)))
     }
 
     /// Devuelve None si no existe la clave, y SystemTime::UNIX_EPOCH si era persistente. Sino, devuelve el valor previo de ttl.
@@ -62,6 +58,10 @@ impl<K: Clone + Eq + Hash, V> TtlHashMap<K, V> {
 
     /// Elimina el time-to-live de la clave, devolviendo el timestamp de expiración. Si no tenía una expiración, devuelve None.
     pub fn delete_ttl(&mut self, key: &K) -> Option<SystemTime> {
+        if self.expired(key) {
+            self.remove(key);
+            return None;
+        }
         self.ttls.remove(key)
     }
 
@@ -136,6 +136,7 @@ impl<K: Clone + Eq + Hash, V> TtlHashMap<K, V> {
 }
 
 impl<K: Eq + Hash + fmt::Display, V: fmt::Display> TtlHashMap<K, V> {
+    /// Devuelve un string con todos los elementos serializados. Los ttl se guardan como segundos desde UNIX_EPOCH.
     pub fn serialize(&self) -> String {
         let mut s = "".to_string();
         for (key, value) in self.store.iter() {
@@ -153,6 +154,7 @@ impl<K: Eq + Hash + fmt::Display, V: fmt::Display> TtlHashMap<K, V> {
 }
 
 impl TtlHashMap<String, RedisElement> {
+    // Deserializa un string para devolver un TtlHashMap cargado con todos los elementos. Solo está implementado para cargar RedisElements.
     pub fn deserialize(s: String) -> Result<Self, String> {
         let mut map: TtlHashMap<String, RedisElement> = TtlHashMap::new();
 
@@ -178,5 +180,158 @@ impl TtlHashMap<String, RedisElement> {
             }
         }
         Ok(map)
+    }
+}
+
+mod test {
+    use crate::entities::ttl_hash_map::RedisElement;
+    use crate::entities::ttl_hash_map::TtlHashMap;
+    use std::time::{Duration, SystemTime};
+
+    #[test]
+    fn test_get_key() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+
+        assert_eq!(map.get(&key).unwrap(), &1);
+    }
+
+    #[test]
+    fn test_get_mut_key_modifies_key() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        let value = map.get_mut(&key).unwrap();
+        *value += 1;
+
+        assert_eq!(map.get(&key).unwrap(), &2);
+    }
+
+    #[ignore]
+    #[test]
+    fn test_ttl_relative_deletes_key() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        map.set_ttl_relative(key.clone(), Duration::from_secs(1));
+
+        std::thread::sleep(Duration::from_secs(1));
+
+        assert!(map.get(&key).is_none());
+    }
+
+    #[test]
+    fn test_ttl_absolute_deletes_key() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        map.set_ttl_absolute(key.clone(), SystemTime::now());
+
+        assert!(map.get(&key).is_none());
+    }
+
+    #[test]
+    fn test_contains_key_on_expired_key() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        map.set_ttl_absolute(key.clone(), SystemTime::now());
+
+        assert!(!map.contains_key(&key));
+    }
+
+    #[test]
+    fn test_delete_ttl_on_expired_key() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        map.set_ttl_absolute(key.clone(), SystemTime::now());
+
+        assert!(map.delete_ttl(&key).is_none());
+        assert!(map.get(&key).is_none());
+    }
+
+    #[test]
+    fn test_delete_ttl_on_presistent_key() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+
+        assert!(map.delete_ttl(&key).is_none());
+        assert!(map.get(&key).is_some());
+    }
+
+    #[test]
+    fn test_insert_key_twice_deletes_ttl() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        map.set_ttl_absolute(key.clone(), SystemTime::now());
+        map.insert(key.clone(), 1);
+
+        assert!(map.delete_ttl(&key).is_none());
+        assert!(map.get(&key).is_some());
+    }
+
+    #[ignore]
+    #[test]
+    fn test_new_key_last_access() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        std::thread::sleep(Duration::from_secs(1));
+
+        assert_eq!(map.update_last_access(&key).unwrap().as_secs(), 1);
+    }
+
+    #[ignore]
+    #[test]
+    fn test_get_changes_last_access() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), 1);
+        std::thread::sleep(Duration::from_secs(1));
+        map.get(&key);
+
+        assert_eq!(map.update_last_access(&key).unwrap().as_secs(), 0);
+    }
+
+    #[test]
+    fn test_remove_key_and_add_again() {
+        let mut map: TtlHashMap<String, u8> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        assert!(map.remove(&key).is_none());
+    }
+
+    #[test]
+    fn test_serialize_and_deserialize() {
+        let mut map: TtlHashMap<String, RedisElement> = TtlHashMap::new();
+        let key = "key".to_string();
+
+        map.insert(key.clone(), RedisElement::String("value".to_string()));
+        map.set_ttl_relative(key.clone(), Duration::from_secs(1));
+
+        let ttl = (SystemTime::now() + Duration::from_secs(1))
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            .to_string();
+        let serialization = map.serialize();
+        assert_eq!(serialization, "key,value,".to_owned() + &ttl + "\n");
+
+        let mut new_map = TtlHashMap::deserialize(serialization).unwrap();
+        assert_eq!(new_map.get(&key).unwrap().to_string(), "value");
     }
 }
