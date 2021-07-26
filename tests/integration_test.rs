1ea13857b684b85148c85b6737647b7fe2c7507d
tomasarrachea <tarrachea@fi.uba.ar>
2021-07-26 13:57:10 -0300

    Cambio estructura del proyecto

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,32 +1,27 @@
-// extern crate proyecto_taller_1;
+use proyecto_taller_1::run_redis;
 extern crate redis;
 
 #[test]
 fn test_set_then_get() {
-    let server = Server::new(Config::new()).unwrap();
+    run_redis(vec![]).unwrap();
 
-    let client = redis::Client::open("redis://localhost:6379").unwrap();
+    let client = redis::Client::open("redis://localhost:6379/").unwrap();
+    assert!(false);
     let mut con = client.get_connection().unwrap();
 
-    server.serve().unwrap();
-    let _set = redis::cmd("SET")
+    let _set: () = redis::cmd("SET")
         .arg("key")
         .arg(42)
         .query(&mut con)
         .unwrap();
-    let get: i32 = redis::cmd("GET")
-        .arg("key")
-        .arg(42)
-        .query(&mut con)
-        .unwrap();
+    let get: i32 = redis::cmd("GET").arg("key").query(&mut con).unwrap();
 
     assert_eq!(get, 42);
 }
 
 #[test]
 fn test_pubsub() {
-    let server = Server::new(Config::new()).unwrap();
-    server.serve().unwrap();
+    run_redis(vec![]).unwrap();
 
     let client = redis::Client::open("redis://localhost:6379").unwrap();
     let mut con = client.get_connection().unwrap();
